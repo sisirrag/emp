fa54034f88ee034d0f4d0ed7acc25eb4c5021698
sisirrag <mca549@rajagiri.edu>
2021-03-17 15:49:12 +0530

    m

---
+++ resources/views/about.blade.php
@@ -0,0 +1,4 @@
+@extends("theme")
+@section("content")
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex asperiores, ratione suscipit sint exercitationem placeat repellat. Delectus quam sequi, vel, nobis nesciunt, sed ducimus dolore doloribus architecto enim ut eaque.
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ Route::get('/', function () {
 Route::get('/searchemp', function () {
     return view('searchemp');
 });
+Route::get('/about', function () {
+    return view('about');
+});
+
